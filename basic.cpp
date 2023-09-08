e9b1f0f3845f1c56b7e0ac21969a4b5b715eb3f5
Marcell Stiedl <marcellstiedl@hotmail.com>
2023-09-08 17:38:34 +0100

    html update

--- basic.cpp
+++ basic.cpp
@@ -9,10 +9,6 @@ int main(int ac, char **av)
         return 1;
     if (av[1])
         {}
-    // struct sockaddr_in addr;
-    // addr.sin_family = AF_UNIX;
-    // addr.sin_port = htons(80);
-    // addr.sin_addr.s_addr = (127U << 24) | (0U << 16) | (0U << 8) | 1U;
     struct addrinfo *addr;
     if (getaddrinfo("127.73.73.7", "7681", NULL, &addr) < 0){ // port 80 to not write everytime the port with the address
         std::cerr << "Error: couldn't get address" << std::endl;
