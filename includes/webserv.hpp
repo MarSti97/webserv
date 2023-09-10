eb07ef0e0bdd11d951583a3f8e267a7d4d9afd90
Dracsis Cella De Cavalli Almeida <dcella-d@c2r3s3.42lisboa.com>
2023-09-10 20:09:11 +0100

    working

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -8,12 +8,15 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
 #include <cstring>
 #include <string>
 #include <fcntl.h>
 #include <errno.h>
 #include <signal.h>
+#include <vector>
+#include <poll.h>
 
 int failToStart(std::string error, struct addrinfo *addr, int socketfd);
 std::string	readFile( std::string filePath );
@@ -21,5 +24,10 @@ std::string getMimeType(const std::string& filePath);
 std::string getURL(char *buffer);
 void	ctrlc(int signum);
 void	printlog(std::string msg, int arg);
+int acceptConnection(int socketfd, struct sockaddr_in *clientinfo, socklen_t &size, std::vector<pollfd> *fds);
+int parseRecv(std::vector<pollfd> &fds, int pos, char *buffer);
+int parseSend(std::vector<pollfd> &fds, int pos, char *buffer);
+std::string getResponse(char *buffer, std::string path, std::string index);
+
 
 #endif
