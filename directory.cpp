1d61a5595299926b7c93f53a75790170e169ee41
Joao Manuel Aiveca Caseiro <jaiveca-@c1r18s1.42lisboa.com>
2023-10-10 18:23:26 +0100

    completed image tests

--- directory.cpp
+++ directory.cpp
@@ -1,6 +1,7 @@
 #include "includes/webserv.hpp"
 
-std::string	make_directory_list()
+std::string	make_directory_list(std::string dirpath)
 {
-	
+	struct dirent *entry;
+	struct stat fileStat;
 }
--- guarder-html/cgi-bin/upload.py
+++ guarder-html/cgi-bin/upload.py
@@ -7,43 +7,19 @@ upload_dir = 'uploads/'
 
 # Create the upload directory if it doesn't exist
 if not os.path.exists(upload_dir):
-    os.makedirs(upload_dir)
+	os.makedirs(upload_dir)
     
-input_data = sys.stdin.buffer.read()
-output_filename = "1.png"
+req_body_data = sys.stdin.buffer.read()
 
-with open(output_filename, 'wb') as output_file:
-    output_file.write(input_data)
-# # Create an instance of FieldStorage to handle the uploaded file
-# form = cgi.FieldStorage()
+output_filename = os.environ.get("BODY_FILENAME")
+file_path = upload_dir + output_filename 
 
-# # Check if the 'file' field exists in the form
-# if 'file' in form:
-#     file_item = form['file']
-    
-#     # Check if the file was successfully uploaded
-#     if file_item.filename:
-#         # Build the absolute path to save the file
-#         file_path = os.path.join(upload_dir, os.path.basename(file_item.filename))
-        
-#         # Open a new file in binary write mode and save the uploaded file
-#         with open(file_path, 'wb') as file:
-#             file.write(file_item.file.read())
-        
-#         print("Content-Type: text/html\n")
-#         print("<html><body>")
-#         print("<h2>File successfully uploaded and saved:</h2>")
-#         print(f"<p>File name: {file_item.filename}</p>")
-#         print(f"<p>File size: {os.path.getsize(file_path)} bytes</p>")
-#         print(f"<p>File location: {file_path}</p>")
-#         print("</body></html>")
-#     else:
-#         print("Content-type: text/html\n")
-#         print("<html><body>")
-#         print("<h2>Error: No file was uploaded.</h2>")
-#         print("</body></html>")
-# else:
-#     print("Content-type: text/html\n")
-#     print("<html><body>")
-#     print("<h2>Error: 'file' field not found in the form.</h2>")
-#     print("</body></html>")
+with open(file_path, 'wb') as output_file:
+    output_file.write(req_body_data)
+
+print("<html><body>")
+print("<h2>File successfully uploaded and saved:</h2>")
+print(f"<p>File name: {output_filename}</p>")
+print(f"<p>File size: {os.path.getsize(file_path)} bytes</p>")
+print(f"<p>File location: {file_path}</p>")
+print("</body></html>")
